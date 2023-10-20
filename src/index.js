1c7685132b4e8715d0dc44fa5af298e9caba4514
joão melo <j@melo.plus>
2023-10-20 15:40:22 -0300

    feat: flexible patterns

--- src/index.js
+++ src/index.js
@@ -1,66 +1,82 @@
+import { minimatch } from "minimatch";
+
 export function initApp() {
   const button = document.getElementById("open");
   button.onclick = doIt;
 }
 
 async function doIt() {
-  const items = [];
-  const rootDir = await showDirectoryPicker();
-  await processDir(rootDir, "", items);
+  const dir = await showDirectoryPicker();
+  updateProcessing();
+  const match = createMatch();
+  const files = await collectFiles({ dir, prefix: "", match });
+  updateList(files);
+}
 
-  const table = document.getElementById("table");
+function createMatch() {
+  const includeValue = document.getElementById("include").value;
+  const include = (name) => minimatch(name, includeValue);
+  const ignoreValue = document.getElementById("ignore").value;
+  const ignore = (name) => minimatch(name, ignoreValue);
+  return { include, ignore };
+}
+
+async function collectFiles({ dir, prefix, match }) {
+  const items = [];
+  for await (const [name, handle] of dir.entries()) {
+    const path = prefix ? prefix + "/" + name : name;
+    if (match.ignore(path)) continue;
+    if (handle.kind === "file" && match.include(path)) {
+      const loc = await getLocs(handle);
+      items.push({ name: path, loc });
+    } else if (handle.kind === "directory") {
+      const dirItems = await collectFiles({ dir: handle, prefix: path, match });
+      items.push(...dirItems);
+    }
+  }
+  return items;
+}
+
+function updateProcessing() {
+  const output = document.getElementById("output");
+  output.innerHTML = "";
+  output.textContent = "processing...";
+}
+
+function updateList(files) {
+  const output = document.getElementById("output");
+  output.innerHTML = "";
+
+  const table = document.createElement("table");
+  output.appendChild(table);
 
   const titles = document.createElement("tr");
-  ["name", "complexity"].forEach((str) => {
-    const title = document.createElement("td");
+  ["name", "loc"].forEach((str) => {
+    const title = document.createElement("th");
     title.textContent = str;
     titles.appendChild(title);
   });
   table.appendChild(titles);
 
-  items.forEach((item) => {
+  files.forEach((item) => {
     const tr = document.createElement("tr");
 
     const name = document.createElement("td");
     name.textContent = item.name;
     tr.appendChild(name);
 
-    const complexity = document.createElement("td");
-    complexity.textContent = item.level;
-    tr.appendChild(complexity);
+    const loc = document.createElement("td");
+    loc.textContent = item.loc;
+    tr.appendChild(loc);
 
     table.appendChild(tr);
   });
 }
 
-async function processDir(dir, suffix, items) {
-  for await (const [name, handle] of dir.entries()) {
-    const newSuffix = suffix + "/" + name;
-
-    if (handle.kind === "file") {
-      if (isCode(name)) {
-        const complexity = await getComplexity(handle);
-        items.push({ name: newSuffix, complexity });
-      }
-    } else if (handle.kind === "directory") {
-      await processDir(handle, newSuffix, items);
-    }
-  }
-}
-
-function isCode(name) {
-  if (["index.scss", "index.js"].includes(name)) return false;
-  const index = name.lastIndexOf(".");
-  if (index === -1) return false;
-  const ext = name.substring(index);
-  return [".js", ".scss", ".vue"].includes(ext);
-}
-
-async function getComplexity(handle) {
+async function getLocs(handle) {
   const file = await handle.getFile();
   const text = await file.text();
   const lines = text.split("\n");
   const loc = lines.length;
-
-  return loc <= 10 ? "simple" : loc <= 50 ? "medium" : "high";
+  return loc;
 }
